93d6db0ce401c2fe6bbc7c28f6510b9584364b2b
Umesh Patel <Umesh.Code1@gmail.com>
2025-07-23 11:44:04 +0530

    Update Project.sol

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -16,15 +16,24 @@ contract SubscriptionService {
         _;
     }
 
+    // Function to subscribe by paying the fee
     function subscribe() external payable {
         require(msg.value == subscriptionFee, "Incorrect subscription fee");
         subscribers[msg.sender] = block.timestamp;
     }
 
+    // Check if a user is subscribed
     function isSubscribed(address _user) external view returns (bool) {
         return subscribers[_user] > 0;
     }
 
+    // NEW: Get the timestamp of when a user subscribed
+    function subscriptionTimestamp(address _user) external view returns (uint256) {
+        require(subscribers[_user] > 0, "User is not subscribed");
+        return subscribers[_user];
+    }
+
+    // Withdraw collected funds (only owner)
     function withdraw() external onlyOwner {
         payable(owner).transfer(address(this).balance);
     }
